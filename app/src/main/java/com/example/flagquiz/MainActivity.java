7dea4abe47ab8ad48479637d4886ca68f803fa14
davadeola <adeoladavid15@gmail.com>
2020-03-11 15:11:00 +0300

    Modified the MainActivity class

--- app/src/main/java/com/example/flagquiz/MainActivity.java
+++ app/src/main/java/com/example/flagquiz/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.flagquiz;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +12,23 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+
+
+    private boolean phoneDevice = true;
+    private boolean preferencesChanged = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +38,64 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+        //set default values in the apps SharedPreferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+
+        //SET listener for SharedPreferences Changes
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(preferencesChangedListener);
+
+        //determine screen size
+        int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+
+
+        //IF device is tablet, set phoneDeviceSize to false
+        if(screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE || screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE){
+            phoneDevice = false;
+
+        }
+
+
+        //if running on PhoneSized device, allow only portrait orientation
+
+        if (phoneDevice)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (preferencesChanged){
+            // now that the default preferences have been set,
+             // initialize MainActivityFragment and start the quiz
+
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+            quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+
+            quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+
+            quizFragment.resetQuiz();
+
+            preferencesChanged = false;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        int orientation  = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT){
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }else{
+            return false;
+        }
+
+
     }
 
     @Override
@@ -36,13 +103,40 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
 
         return super.onOptionsItemSelected(item);
+
     }
+
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangedListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            preferencesChanged = true;
+
+            MainActivityFragment quizFragment = (MainActivityFragment)  getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+            if (key.equals(CHOICES)){
+                quizFragment.updateGuessRows(sharedPreferences);
+                quizFragment.resetQuiz();
+            }else if(key.equals(REGIONS)){
+                Set<String> regions = sharedPreferences.getStringSet(REGIONS, null);
+
+
+                if (regions != null && regions.size()> 0){
+                    quizFragment.updateRegions(sharedPreferences);
+                    quizFragment.resetQuiz();
+                }else{
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    regions.add(getString(R.string.default_region));
+                    editor.putStringSet(REGIONS, regions);
+                    editor.apply();
+
+                    Toast.makeText(MainActivity.this, R.string.default_region_message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
 }
